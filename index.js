5556f15374db3d90bd8e3050a5e8ba631d4c5631
SRIJAN MISHRA <152499694+Srijan-namaste@users.noreply.github.com>
2024-06-27 03:26:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,19 +24,25 @@ if(isNaN(guess)){
     }else{
         window.alert(`CORRECT! The answer was ${answer}. It took you ${attempts} attempts`);
         if(attempts == 1){
-            window.alert("Your luck score is 7/7.. Are you sure you're not GOD");
+            window.alert("Your luck score is 10/10.. Are you sure you're not GOD");
         }else if(attempts == 2){
-            window.alert("Your luck score is 6/7.. With your luck, you can find a four-leaf clover in a field of daisies");
+            window.alert("Your luck score is 9/10.. With your luck, you can find a four-leaf clover in a field of daisies");
         }else if(attempts == 3){
-            window.alert("Your luck score is 5/7.. Your are lucky enough to find a parking spot right in front of the entrance.");
+            window.alert("Your luck score is 8/10.. Your are lucky enough to find a parking spot right in front of the entrance.");
         }else if(attempts == 4){
-            window.alert("Your luck score is 4/7.. You have lucky enough to find a penny on the sidewalk when you need a bit of change");
+            window.alert("Your luck score is 7/10.. You have lucky enough to find a penny on the sidewalk when you need a bit of change");
         }else if(attempts == 5){
-            window.alert("Your luck score is 3/7.. Your score is above 85% of people.. So many unfortunates right?");
+            window.alert("Your luck score is 6/10.. You are lucky enough to catch green lights on every intersection");
         }else if(attempts == 6){
-            window.alert("Your luck score is 2/7.. Using maths to get your luck number?");
+            window.alert("Your luck score is 5/10.. Your score is above 85% of people.. So many unfortunates.. right?");
         }else if(attempts == 7){
-            window.alert("Your luck score is 1/7.. You are one of those who do not believe in luck but in maths");
+            window.alert("Your luck score is 4/10.. You are one of those who do not believe in luck but in maths");
+        }else if(attempts == 8){
+            window.alert("Your luck score is 3/10.. Math can only take you so far");
+        }else if(attempts == 9){
+            window.alert("Your luck score is 2/10.. Pray harder.. getting low on luck coins");
+        }else if(attempts == 10){
+            window.alert("Your luck score is 1/10.. You might need some 🍀");
         }else{
             window.alert("You are running out of luck. Try maths for better outcome.. Checkout the readme text");
         }
